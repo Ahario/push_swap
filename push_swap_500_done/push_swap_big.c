dd21b5f3fa1e833508d328b888914c6de9b04543
Ahario <81345604+Ahario@users.noreply.github.com>
2022-07-05 15:58:15 +0900

    Add files via upload

---
+++ push_swap_500_done/push_swap_big.c
@@ -0,0 +1,207 @@
+#include "push_swap.h"
+
+void single_chunk_ra(int target, t_push_swap *ps_a, t_push_swap *ps_b)
+{
+    int i;
+    int half;
+
+    half = ps_a->total / 2;
+    i = 0;
+    while(i < half)
+    {
+        while(ps_a->stack[0] > target)
+        {
+            if (ps_b->stack[0] < ps_a->complete_stack[ps_a->total / 4] && ps_b->array_total > 1)
+                rr(ps_a, ps_b);
+            else
+                ra(ps_a);
+        }
+        pb(ps_a, ps_b);
+        i++;
+    }
+}
+
+int check_bigger(int *half, t_push_swap *ps_b)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = ps_b->array_total ;
+    if (j < 0)
+        return (1);
+    while(i != j)
+    {
+        if(ps_b->stack[i] > ps_b->complete_stack[half[1]])
+            return(0);
+        i++;
+    }
+    return (1);
+}
+
+void push_from_stack_a(int *half, t_push_swap *ps_a, t_push_swap *ps_b)
+{
+    int i;
+    int j;
+
+    j = 0;
+    i = 0;
+    if (half[0] == half[1])
+        ft_memmove(half, ps_a);
+    while(ps_a->stack[0] < ps_a->complete_stack[half[1]])
+        ra(ps_a);
+    ft_memmove(half, ps_a);
+    if(ps_a->complete_stack[half[0]] == ps_a->complete_stack[ps_a->total - 1])
+    {
+        ra(ps_a);
+        return ;
+    }
+    half[ps_a->half_total] = (half[1] + half[0]) / 2;
+    ps_a->half_total++;
+    sort_my_array(half, ps_a->half_total);
+    if (sort_complete(ps_a->complete_stack, ps_a) == 0)
+    {
+        while(ps_a->complete_stack[half[2]] >= ps_a->stack[0] && ps_a->complete_stack[half[0]] <= ps_a->stack[0])
+        {
+                if(ps_a->stack[0] > ps_a->complete_stack[half[1]])
+                {
+                    if (ps_b->stack[0] < ps_a->complete_stack[half[1]] && ps_b->array_total > 1)
+                        rr(ps_a, ps_b);
+                    else
+                        ra(ps_a);
+                    j++;
+                }
+                else
+                    pb(ps_a, ps_b);
+        }
+        while(j != 0)
+        {
+            rra(ps_a);
+            j--;
+        }
+        if(ps_b->array_total <= 40)
+            push_from_stack_b_less_than_20(half, ps_a, ps_b);
+        else
+            push_from_stack_b(half, ps_a, ps_b);
+    }
+    if (sort_complete(ps_a->complete_stack, ps_a) == 0)
+        push_from_stack_a(half, ps_a, ps_b);
+}
+
+void max_num_pa(int k, t_push_swap *ps_a, t_push_swap *ps_b)
+{
+    while(ps_b->stack[0] < ps_b->complete_stack[k])
+    {
+        if(find_minimum_ra_rra_500(ps_b->complete_stack[k], ps_b) == 1)
+            rb(ps_b);
+        else
+            rrb(ps_b);
+    }
+    if (ps_b->stack[0] == ps_b->complete_stack[k])
+        pa(ps_a, ps_b);
+}
+
+void min_num_pa(int i, t_push_swap *ps_a, t_push_swap *ps_b)
+{
+    int j;
+    int flag;
+
+    j = i;
+    flag = 0;
+    while(ps_b->stack[0] > ps_b->complete_stack[i])
+    {
+        if(ps_b->stack[0] == ps_b->complete_stack[j + 1] && flag == 0)
+        {
+            pa(ps_a, ps_b);
+            flag = 1;
+        }
+        else if(find_minimum_ra_rra_500(ps_b->complete_stack[i], ps_b) == 1)
+            rb(ps_b);
+        else if (find_minimum_ra_rra_500(ps_b->complete_stack[i], ps_b) != 1)
+            rrb(ps_b);
+    }
+    if (ps_b->stack[0] == ps_b->complete_stack[i])
+    {
+        pa(ps_a, ps_b);
+        ra(ps_a);
+        if (flag == 1)
+            ra(ps_a);
+    }
+}
+
+void push_from_stack_b_less_than_20(int *half, t_push_swap *ps_a, t_push_swap *ps_b)
+{
+    int i;
+    int k;
+
+    i = half[0];
+    k = half[1];
+    while(k > i)
+    {
+        if (find_minimum_ra_rra_600(ps_b->complete_stack[k], ps_b) <= find_minimum_ra_rra_600(ps_b->complete_stack[i], ps_b))
+        {
+            max_num_pa(k, ps_a, ps_b);
+            k--;
+        }
+        else
+        {
+            min_num_pa(i, ps_a, ps_b);
+            i++;
+            if (ps_a->complete_stack[i] == ps_a->stack[ps_a->array_total - 1])
+                i++;
+        }
+    }
+    if (ps_b->array_total == 1)
+        pa(ps_a, ps_b);
+    return;
+}
+
+void push_from_stack_b(int *half, t_push_swap *ps_a, t_push_swap *ps_b)
+{
+    int i;
+    int j;
+
+    half[ps_a->half_total] = (half[0] + half[1]) / 2;
+    ps_a->half_total++;
+    sort_my_array(half, ps_a->half_total);
+    i = half[0];
+    j = half[1];
+
+    while(check_bigger(half, ps_b) == 0 || (i == half[0] && j == half[0]))
+    {
+        while(ps_b->stack[0] <= ps_b->complete_stack[half[1]])
+            rb(ps_b);
+        if (ps_b->stack[0] > ps_b->complete_stack[half[1]])
+            pa(ps_a, ps_b);
+        i++;
+    }
+    if (ps_b->array_total == 1)
+    {
+        pa(ps_a, ps_b);
+        return;
+    }
+    if (ps_b->array_total != 1)
+        push_from_stack_b(half, ps_a, ps_b);
+}
+
+void push_swap_single_chunk(int *my_array, t_push_swap *ps_a, t_push_swap *ps_b)
+{
+    int *half;
+    int half_value;
+
+    half = malloc(sizeof(int) * ps_a->total);
+    half_value = my_array[(ps_a->total / 2) - 1];
+    half[0] = 0;
+    half[1] = (ps_a->total / 2) - 1;
+    half[2] = ps_a->total - 1;
+    ps_a->half_start = 0;
+    ps_a->half_end = 2;
+    ps_a->half_total = 3;
+    ps_b->half_total = 3;
+    single_chunk_ra(half_value, ps_a, ps_b);
+    if (ps_b->array_total <= 100)
+        push_from_stack_b_less_than_20(half, ps_a, ps_b);
+    else
+        push_from_stack_b(half, ps_a, ps_b);
+    push_from_stack_a(half, ps_a, ps_b);
+}
---
+++ push_swap_500_done/push_swap_hc.c
@@ -0,0 +1,89 @@
+#include "push_swap.h"
+
+void less_than_three(int *my_array, t_push_swap *ps_a)
+{
+    copy_array_to_temp(ps_a);
+    if (!(sort_complete(my_array, ps_a)))
+    {
+    	rra_no_write(ps_a);
+        if (sort_complete_temp(my_array, ps_a))
+        {
+            rra(ps_a);
+            return ;
+        }
+        ra_no_write(ps_a);
+        ra_no_write(ps_a);
+        if (sort_complete_temp(my_array, ps_a))
+        {
+            ra(ps_a);
+            return ;
+        }
+        rra_no_write(ps_a);
+        sa(ps_a);
+        if (!sort_complete(my_array, ps_a))
+            less_than_three(my_array, ps_a);
+        return ;
+    }
+}
+
+void less_than_five(int *my_array, t_push_swap *ps_a, t_push_swap *ps_b)
+{
+    int *temp;
+
+    temp = 0;
+    if(!(sort_complete(my_array, ps_a)))
+    {  
+        if (ps_a->array_total > 3)
+        {
+            find_minimum_ra_rra(my_array[0], ps_a);
+            pb(ps_a, ps_b);
+            find_minimum_ra_rra(my_array[1], ps_a);
+            pb(ps_a, ps_b);
+        }
+        temp = ft_strdup_int(&my_array[2], ps_a->array_total);
+        less_than_three(temp, ps_a);
+        pa(ps_a, ps_b);
+        pa(ps_a, ps_b);
+    }
+}
+
+void find_minimum_ra_rra(int target, t_push_swap *ps)
+{
+    int ra_count;
+    int rra_count;
+
+    ra_count = 0;
+    rra_count = 0;
+    copy_array_to_temp(ps);
+    while(ps->temp_stack[0] != target)
+    {
+        ra_no_write(ps);
+        ra_count++;
+    }
+    copy_array_to_temp(ps);
+    while(ps->temp_stack[0] != target)
+    {
+        rra_no_write(ps);
+        rra_count++;
+    }
+    if(ra_count <= rra_count)
+        ra_repeat(target, ps);
+    else
+        rra_repeat(target, ps);
+}
+
+void ra_repeat(int target, t_push_swap *ps)
+{
+    while(ps->stack[0] != target)
+        ra(ps);
+    if (sort_complete(ps->complete_stack, ps))
+        exit(1);
+}
+
+void rra_repeat(int target, t_push_swap *ps)
+{
+    while(ps->stack[0] != target)
+        rra(ps);
+    if (sort_complete(ps->complete_stack, ps))
+        exit(1);
+}
